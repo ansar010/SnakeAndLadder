70c0d8776940f326663c592f229ae3a492c7b2ba
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-22 21:06:36 +0530

    Rolling the die feature added

--- SnakeLadderEngine.java
+++ SnakeLadderEngine.java
@@ -1,8 +1,17 @@
 package snakeladder;
+import java.util.Random;
 import snakeladder.Player;
 
 public class SnakeLadderEngine{
     public static void loader(){
         Player p1 = new Player();
+        start(p1);
+    }
+    public static void start(Player p1){
+        int dieCount = throwDie();
+    }
+    private static int throwDie(){
+        Random rand = new Random();
+        return rand.nextInt(6) + 1;
     }
 }
