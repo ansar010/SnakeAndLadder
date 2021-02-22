18b3b825641cfd675eb500525c4bc23e08e5e56f
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-22 21:24:23 +0530

    Added moves of the player

--- Player.java
+++ Player.java
@@ -2,7 +2,31 @@ package snakeladder;
 
 public class Player{
     private int position;
+    private int totNoMoves;
     Player(){
         position = 0;
+        totNoMoves = 0;
+    }
+    
+    public void move(int opt){
+        if(opt > 0){
+            position += opt;
+            totNoMoves++;
+        }
+        else{
+            if(position == 0)
+                return;
+            if(position - opt < 0)
+                position = 0;
+            else
+                position -= opt;
+            totNoMoves--;
+        }
+    }
+    public int getPosition(){
+        return position;
+    }
+    public int getTotalMoves(){
+        return totNoMoves;
     }
 }
--- SnakeLadderEngine.java
+++ SnakeLadderEngine.java
@@ -7,11 +7,26 @@ public class SnakeLadderEngine{
         Player p1 = new Player();
         start(p1);
     }
-    public static void start(Player p1){
+    private static void start(Player p1){
         int dieCount = throwDie();
+        int optAhead = giveOptions(dieCount);
+        p1.move(optAhead);
     }
     private static int throwDie(){
         Random rand = new Random();
         return rand.nextInt(6) + 1;
+    }
+    private static int giveOptions(int dieCount){
+        Random rand = new Random();
+        int opt = rand.nextInt(3);
+        switch(opt){
+            case 0: 
+                return 0;
+            case 1:
+                return dieCount;
+            case 2:
+                return (-1)*dieCount;
+        }
+        return -1;
     }
 }
